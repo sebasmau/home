a797395709f168e5c5e591639795497b4bedd43e
Sebastien Mauroo <sebastien.mauroo@outlook.com>
2023-02-19 18:40:56 +0100

    update requirements

--- dashboard/pages/20_📚_Streamlit_library.py
+++ dashboard/pages/20_📚_Streamlit_library.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import datetime as dt
+import altair as alt
+import time
+import pydeck as pdk
+import datetime
+from urllib.error import URLError
 
 
 st.set_page_config(
