32137872672614ac5c884a15a44eb250686e8268
Sebastien Mauroo <sebastien.mauroo@outlook.com>
2023-02-19 17:23:14 +0100

    toglle streamlit hidden style

--- dashboard/pages/_⚙️_Settings.py
+++ dashboard/pages/_⚙️_Settings.py
@@ -12,13 +12,13 @@ st.set_page_config(
 with open('dashboard/style.css') as f:
     st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+# hide_streamlit_style = """
+#             <style>
+#             #MainMenu {visibility: hidden;}
+#             footer {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 
 
