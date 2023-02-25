f2ca3b0ae67e152f8c759150f43ed2d27a49f7ee
Sebastien Mauroo <sebastien.mauroo@outlook.com>
2023-02-25 11:17:46 +0100

    simplified code, no different pyrebase class

--- dashboard/1_🏠_Home.py
+++ dashboard/1_🏠_Home.py
@@ -1,5 +1,7 @@
 import streamlit as st
-import StreamlitAppBlocks 
+import StreamlitAppBlocks
+import pyrebase
+import time
 
 ####PAGE CONFIG
 
@@ -10,11 +12,33 @@ st.set_page_config(
 )
 
 
+####LOGIN CODE####
+
+if 'firebase' not in st.session_state:
+        config = {"apiKey":st.secrets["firebase_credentials"]["apiKey"],"authDomain":st.secrets["firebase_credentials"]["authDomain"],"storageBucket":st.secrets["firebase_credentials"]["storageBucket"],"databaseURL":st.secrets["firebase_credentials"]["databaseURL"]}
+        st.session_state['firebase'] = pyrebase.initialize_app(config)
+
 if 'logged_in' not in st.session_state:
         st.session_state['logged_in'] = False
 
-if st.session_state['logged_in'] == False:
-    StreamlitAppBlocks.login_screen()
+if st.session_state['logged_in'] == False:   
+    with st.form("Inloggen"):
+        st.subheader("Login")
+        email = st.text_input("Emailadres")
+        pw = st.text_input("Wachtwoord", type="password")
+        login = st.form_submit_button("Login")
+        
+    if login:
+        try:
+            signin = st.session_state['firebase'].auth().sign_in_with_email_and_password(email,pw)
+            st.success("Login succesvol")
+            st.session_state['logged_in'] = True
+            time.sleep(3)
+            st.experimental_rerun()
+        except:
+            st.error("verkeerd wachtwoord")
+    st.stop()
+
 
 
 #### add CSS style and hide unneeded streamlit visuals
