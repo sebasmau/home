1d691ab3cfcd7a83a025f6e4647b5d11c0921130
Sebastien Mauroo <sebastien.mauroo@outlook.com>
2023-02-25 10:38:34 +0100

    login test

--- dashboard/StreamlitAppBlocks.py
+++ dashboard/StreamlitAppBlocks.py
@@ -2,10 +2,26 @@ import streamlit as st
 import pyrebase
 
 
+def init_firebase(config):
+    if 'firebase' not in st.session_state:
+        st.session_state['firebase'] = pyrebase.initialize_app(config)
+
 def login_screen():
 
+    init_firebase()
+    
+    if 'logged_in' not in st.session_state:
+        st.session_state['logged_in'] = False
+    
     with st.form("Inloggen"):
         st.subheader("Login")
         email = st.text_input("Emailadres")
         pw = st.text_input("Wachtwoord", type="password")
         login = st.form_submit_button("Login")
+    
+    if login:
+        try:
+            signin = st.session_state['firebase'].auth().sign_in_with_email_and_password(email,pw)
+            st.succes("Login succesvol")
+        except:
+            st.succes("verkeerd wachtwoord")
